109bab9d860f62152e83f9de869ec81a57fb362d
Shaggggy_ <79113250+jazdzewt@users.noreply.github.com>
2024-04-14 18:22:10 +0200

    Update read_to_array.c

--- read_to_array.c
+++ read_to_array.c
@@ -16,40 +16,48 @@ void read_to_array (FILE* file, int height, int width ){
     
     rewind(file); 
 
-    char** array = malloc(MAX_MODULE_SIZE * sizeof(char*));
+    char** array = malloc((MAX_MODULE_SIZE) * sizeof(char*));
 
-    for (int i = 0; i < MAX_MODULE_SIZE; i++){
+    for (int i = 0; i <= MAX_MODULE_SIZE; i++){
         array[i] = malloc(width * sizeof(char));
     }
 
-    for (int i = 0; i< MAX_MODULE_SIZE; i++){
+    for (int i = 0; i<= MAX_MODULE_SIZE; i++){
         for (int j = 0; j< width  + 1; j++){
             array[i][j] = (char) fgetc(file); 
         }
     }
+    
+    for(int i = 0; i < width; i++){
+        if(array[MAX_MODULE_SIZE][i] == ' '){
+            array[MAX_MODULE_SIZE][i] = 'Z';
+        }
+    }
 
     //
-    while(zalewanie(&array, MAX_MODULE_SIZE, width));
+    while(zalewanie(&array, MAX_MODULE_SIZE+1, width));
 
     //printowanie mapy 
-    for (int j = 0; j < MAX_MODULE_SIZE; j++){
+    for (int j = 0; j <= MAX_MODULE_SIZE; j++){
         for(int h = 0; h < width; h++){
             printf("%c", array[j][h]);
         }
         printf("\n");
     }
 
+    printf("\n");
+
     bfsv2(&array, MAX_MODULE_SIZE, width, tmp_file, module_number);
 
 
     //printowanie mapy 
-    for (int j = 0; j < MAX_MODULE_SIZE; j++){
+    for (int j = 0; j <= MAX_MODULE_SIZE; j++){
         for(int h = 0; h < width; h++){
-            printf(" [%c] ", array[j][h]);
+            printf("%c", array[j][h]);
         }
         printf("\n");
     }
-
+    /*
     for (int i = 2; i <= module_vol; i++){
         module_number = i; 
 
@@ -62,16 +70,16 @@ void read_to_array (FILE* file, int height, int width ){
                 array[i][j] = (char) fgetc(file); 
             }
         }
-        /*for (int i = 0; i< width; i++){
-            array[MAX_MODULE_SIZE-1][i] = 'Z';
-        }*/
+        for(int i = 0; i < width; i++){
+            array[MAX_MODULE_SIZE][i] = 'Z';
+        }
+        
         while(zalewanie(&array, MAX_MODULE_SIZE, width));
         bfsv2(&array, MAX_MODULE_SIZE, width, tmp_file, module_number);
     }
+    */
 
-    //
-
-    for (int i = 0; i < MAX_MODULE_SIZE; i++){
+    for (int i = 0; i <= MAX_MODULE_SIZE; i++){
         free(array[i]);
     }
     
